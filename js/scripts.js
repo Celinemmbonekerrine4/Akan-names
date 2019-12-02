e3c24d8f0942adeb0aee72afee3708f77f2092c0
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2019-12-02 09:49:54 +0300

    it will correct the brackets

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ function naming(){
   var MM=parseInt(document.getElementById("Form").value);
   var DD=parseInt(document.getElementById("akan").value);
 
-  var day=("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday");
-  var femaleNames=("Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama");
-  var maleNames=("Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame");
+  var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
+  var femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
+  var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
  
   if(CC.value =="" || YY.value =="" || MM.value =="" || DD.value ==""){
       alert("No blank values allowed");
